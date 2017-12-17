1edebeb7378a790eaefe3f3e38fa260e3fc07e58
greatjack1 <yaakovrim@gmail.com>
2017-12-17 01:09:15 -0500

    added error checking

--- DialogflowFullfillmentTemplate/Controllers/ValuesController.cs
+++ DialogflowFullfillmentTemplate/Controllers/ValuesController.cs
@@ -27,49 +27,59 @@ namespace DialogflowFullfillmentTemplate.Controllers
         [HttpPost]
         public async Task<JsonResult> Post([FromBody]string value)
         {
-         //Create request
-            WebRequest request = WebRequest.Create("http://freegeoip.net/json/" + Request.Headers["x-forwarded-for"].ToString());
-            request.Method = "GET";
-            //Get the response
-            WebResponse wr = await request.GetResponseAsync();
-            Stream receiveStream = wr.GetResponseStream();
-            StreamReader reader = new StreamReader(receiveStream);
-            string content = reader.ReadToEnd();
-            JObject obj = JObject.Parse(content);
-            String city = (String)obj["city"];
-            double lat = (Double)obj["latitude"];
-            double lon = (Double)obj["longitude"];
-            String timeZone = (String)obj["time_zone"];
-            //generate the zmanim
-            ITimeZone zmanimTimeZone = new WindowsTimeZone(timeZone);
-            GeoLocation location = new GeoLocation("Random", lat, lon,
-            0, zmanimTimeZone);
-            ComplexZmanimCalendar czc = new ComplexZmanimCalendar(location);
-            String Speech = "Here are your zmanim for "+ city+". Alos is at " + czc.GetAlosHashachar().formatDate()+". Sunrise is at " + czc.GetSunrise().formatDate()
-              + ". Sof Zeman Krias Shema is at " + czc.GetSofZmanShmaMGA().formatDate() + " According to the Magen Avraham, and at "
-                + czc.GetSofZmanShmaGRA().formatDate() + " according to the Gra." + " Sof zeman Teffilah is at "
-                                                 + czc.GetSofZmanTfilaGRA().formatDate() + " According to the gra and at "
-                                                 + czc.GetSofZmanShmaMGA().formatDate() + " according to the Magen Avraham. "
-                                                 + "Chatzos is at " + czc.GetChatzos().formatDate() + ". Mincha Gedolah is at " + czc.GetMinchaGedola().formatDate()
-                                                 + ". Mincha Ketana is at " + czc.GetMinchaKetana().formatDate() + ". Shkia is at " + czc.GetSunset().formatDate()
-                                                 + ". Tzais Hakochavim is at " + czc.GetTzais().formatDate();
+            //Create request
+            try
+            {
+                WebRequest request = WebRequest.Create("http://freegeoip.net/json/" + Request.Headers["x-forwarded-for"].ToString());
+                request.Method = "GET";
+                //Get the response
+                WebResponse wr = await request.GetResponseAsync();
+                Stream receiveStream = wr.GetResponseStream();
+                StreamReader reader = new StreamReader(receiveStream);
+                string content = reader.ReadToEnd();
+                JObject obj = JObject.Parse(content);
+                String city = (String)obj["city"];
+                double lat = (Double)obj["latitude"];
+                double lon = (Double)obj["longitude"];
+                String timeZone = (String)obj["time_zone"];
+                //generate the zmanim
+                ITimeZone zmanimTimeZone = new WindowsTimeZone(timeZone);
+                GeoLocation location = new GeoLocation("Random", lat, lon,
+                0, zmanimTimeZone);
+                ComplexZmanimCalendar czc = new ComplexZmanimCalendar(location);
+                String Speech = "Here are your zmanim for " + city + ". Alos is at " + czc.GetAlosHashachar().formatDate() + ". Sunrise is at " + czc.GetSunrise().formatDate()
+                  + ". Sof Zeman Krias Shema is at " + czc.GetSofZmanShmaMGA().formatDate() + " According to the Magen Avraham, and at "
+                    + czc.GetSofZmanShmaGRA().formatDate() + " according to the Gra." + " Sof zeman Teffilah is at "
+                                                     + czc.GetSofZmanTfilaGRA().formatDate() + " According to the gra and at "
+                                                     + czc.GetSofZmanShmaMGA().formatDate() + " according to the Magen Avraham. "
+                                                     + "Chatzos is at " + czc.GetChatzos().formatDate() + ". Mincha Gedolah is at " + czc.GetMinchaGedola().formatDate()
+                                                     + ". Mincha Ketana is at " + czc.GetMinchaKetana().formatDate() + ". Shkia is at " + czc.GetSunset().formatDate()
+                                                     + ". Tzais Hakochavim is at " + czc.GetTzais().formatDate();
 
-            String display = 
-         "Zmanim for "+city+"\nAlos-" + czc.GetAlosHashachar().formatDate() + "\nSunrise-" + czc.GetSunrise().formatDate()
-              + "\nSof Zeman Krias Shema MGA-" + czc.GetSofZmanShmaMGA().formatDate() + "\n Sof Zman Krias Shema Gra-"
-                + czc.GetSofZmanShmaGRA().formatDate() + "\nSof zeman Teffilah Gra-"
-                                                 + czc.GetSofZmanTfilaGRA().formatDate() + "\n Sof Zman Tefilah Mga-"
-                                                 + czc.GetSofZmanShmaMGA().formatDate()
-                                                 + "\nChatzos-" + czc.GetChatzos().formatDate() + "\nMincha Gedolah-" + czc.GetMinchaGedola().formatDate()
-                                                 + "\nMincha Ketana-" + czc.GetMinchaKetana().formatDate() + "\nShkia-" + czc.GetSunset().formatDate()
-                                                 + "\nTzais Hakochavim-" + czc.GetTzais().formatDate();
+                String display =
+             "Zmanim for " + city + "\nAlos-" + czc.GetAlosHashachar().formatDate() + "\nSunrise-" + czc.GetSunrise().formatDate()
+                  + "\nSof Zeman Krias Shema MGA-" + czc.GetSofZmanShmaMGA().formatDate() + "\n Sof Zman Krias Shema Gra-"
+                    + czc.GetSofZmanShmaGRA().formatDate() + "\nSof zeman Teffilah Gra-"
+                                                     + czc.GetSofZmanTfilaGRA().formatDate() + "\n Sof Zman Tefilah Mga-"
+                                                     + czc.GetSofZmanShmaMGA().formatDate()
+                                                     + "\nChatzos-" + czc.GetChatzos().formatDate() + "\nMincha Gedolah-" + czc.GetMinchaGedola().formatDate()
+                                                     + "\nMincha Ketana-" + czc.GetMinchaKetana().formatDate() + "\nShkia-" + czc.GetSunset().formatDate()
+                                                     + "\nTzais Hakochavim-" + czc.GetTzais().formatDate();
 
-            JsonResponse response = new JsonResponse();
-            response.DisplayText = display;
-            response.Source = "Zmanim.net";
-            response.Speech = Speech;
-            Console.WriteLine("Got a request from postman");
-            return Json(response);
+                JsonResponse response = new JsonResponse();
+                response.DisplayText = display;
+                response.Source = "Zmanim.net";
+                response.Speech = Speech;
+                Console.WriteLine("Got a request from postman");
+                return Json(response);
+            } catch(Exception ex){
+                //write the error message to console
+                Console.WriteLine(ex.ToString());
+                JsonResponse response = new JsonResponse();
+                response.DisplayText = "There was a error getting jewish times for your location, please try again later";
+                response.Source = "Zmanimaog.herokuapp.com";
+                response.Speech = "There was a error getting jewish times for your location, please try again later";
+            }
         }
        
         // PUT api/values/5
